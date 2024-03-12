ace746682bd5cab139ddf57e53a281e5b43235b9
Andres Rodriguez <carodriguez19@espe.edu.ec>
2024-03-11 20:04:28 -0500

    creadas entidades

--- productos/src/main/java/ec/edu/espe/pruebaparcial3/productos/domain/Producto.java
+++ productos/src/main/java/ec/edu/espe/pruebaparcial3/productos/domain/Producto.java
@@ -1,5 +1,22 @@
 package ec.edu.espe.pruebaparcial3.productos.domain;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document(collection ="producto")
+@Builder
 public class Producto {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String codigoUnico;
+    private String nombre;
+    private String descripcion;
+    private Float precio;
+    private Float existencia;
+
 }
